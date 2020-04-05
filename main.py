cb8ac66b686217ee05260c27273c418cf292836a
Hugo Arboleas <hugo.arboleas@gmail.com>
2020-04-04 22:47:27 -0300

    cambios varios

--- main.py
+++ main.py
@@ -10,7 +10,7 @@
 
 import sys
 import cv2
-from PySide2 import QtCore, QtWidgets
+from PySide2 import QtCore, QtGui, QtWidgets
 import gui
 
 
@@ -20,12 +20,16 @@ class AppWindow(QtWidgets.QDialog, gui.Ui_Dialog):
         super().__init__()
         self.setupUi(self)
 
-        self.camara = cv2.VideoCapture("video.mp4")
+        self.camara = cv2.VideoCapture("video_0.mp4")
+        self.i = 0
 
         # Timer para la captura de un cuadro de video cada 33ms
         self.timer = QtCore.QTimer(self)
         self.timer.timeout.connect(self.read_frame)
         self.timer.start(33)
+
+        # Cargo el XML para reconocimiento facial
+        self.face_detect = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
         self.show()
 
@@ -35,10 +39,35 @@ class AppWindow(QtWidgets.QDialog, gui.Ui_Dialog):
 
         ok, frame = self.camara.read()
 
-        if ok:
-            print("OK")
-        else:
-            self.img.setText("Sin video")
+        if not ok:
+            #Cuando se termina el video, comienzo desde el principio
+            self.camara.release()
+            self.i = (self.i + 1) % 2
+            self.camara = cv2.VideoCapture("video_"+str(self.i)+".mp4")
+            # self.img.setText("Sin video")
+            return
+
+        # Convierto a gray para deteccion de rostros
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        frame[:,:,0] = frame_gray
+        frame[:,:,1] = frame_gray
+        frame[:,:,2] = frame_gray
+        # Detecto rostros y los recuadro
+        a = self.scaleFactor.value()
+        b = self.minNeighbors.value()
+        faces = self.face_detect.detectMultiScale(frame_gray, a, b)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0))
+
+        #Convierto imagen de opencv a qt
+        image = QtGui.QImage(frame, frame.shape[1], frame.shape[0],
+                frame.shape[1]*frame.shape[2], QtGui.QImage.Format_RGB888)
+        pixmap = QtGui.QPixmap()
+        pixmap.convertFromImage(image.rgbSwapped())
+
+        self.img.setPixmap(pixmap)
+
 
     def unlock_conf(self):
         self.lock.setEnabled(True)
@@ -47,6 +76,8 @@ class AppWindow(QtWidgets.QDialog, gui.Ui_Dialog):
         self.temp_max.setEnabled(True)
         self.emisividad.setEnabled(True)
         self.umbral_fiebre.setEnabled(True)
+        self.scaleFactor.setEnabled(True)
+        self.minNeighbors.setEnabled(True)
 
     def lock_conf(self):
         self.lock.setEnabled(False)
@@ -55,6 +86,9 @@ class AppWindow(QtWidgets.QDialog, gui.Ui_Dialog):
         self.temp_max.setEnabled(False)
         self.emisividad.setEnabled(False)
         self.umbral_fiebre.setEnabled(False)
+        self.scaleFactor.setEnabled(False)
+        self.minNeighbors.setEnabled(False)
+
 
 
 if __name__ == "__main__":
